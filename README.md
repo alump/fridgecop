c815461d285878315fc29cb1a13138b2ee4217d7
Sami Viitanen <sami.viitanen@vaadin.com>
2019-08-11 18:28:02 -0700

    Getting there...

--- README.md
+++ README.md
@@ -4,11 +4,37 @@
 
 Me just trying to do simple REST service to call via IFTTT when it receives on/off signals from my IOT door open sensor. Idea is to use this to trigger alarm if fridge door is left open for more than N minutes. This is just fastly written PoC, free free to steal and adapt if useful.
 
+## Version History
+
+### 0.0.1 Initialial Release (TBD)
+- Still under development
+
 ## Usage
 
-### Pull and run locally...
+### Pull and Run Locally...
 To run app you need to have git, node and npm available on your machine.
-> git clone <>
+> git clone https://github.com/alump/fridgecop.git
+
 > cd fridgecop
+
 > npm install
+
 > node app.js
+
+After this you should see "Service started" message.
+
+### Calling Service
+
+#### REST/JSON Calls
+
+To get current status call (GET): http://localhost:3000/status
+
+To send "open" update call (GET): http://localhost:3000/open?secretKey=XXX
+
+To send "closed" update call (GET): http://localhost:3000/closed?secretKey=XXX
+
+To read the latest events (GET): http://localhost:3000/history
+
+#### Browser
+
+There is also page for browser: http://localhost:3000
--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const exphbs  = require('express-handlebars');
 const moment = require('moment-timezone');
 
+const packageJson = require('./package.json');
+
 var app = express();
 
+// TODO: Move following to properties when ready
 const alarmMinutes = 3;
 const timeZone = "America/Los_Angeles";
+const deviceName = "Fridge Door";
+const historySize = 10;
+const secretKey = "todoTODOtodo";
 
 const alarmMilliSecs = alarmMinutes * 60 * 1000;
 
@@ -15,17 +21,43 @@ var alarmTimeout = undefined;
 var lastAlarm = undefined;
 var restCallCounter = 0;
 var doorLastOpen = undefined;
+var doorLastClosed = undefined;
+var eventHistory = [];
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+function addToHistory(eventTimestamp, isOpen) {
+    let historyEvent = {
+        "time": formatMoment(eventTimestamp),
+        "isOpen": isOpen,
+        "newStatus": getOpenStatusString()
+    };
+    if(eventHistory.length >= historySize) {
+        eventHistory.shift();
+    }
+    eventHistory.push(historyEvent);
+}
+
 function setOpenStatus(nowOpen) {
-    let response = {
-        "oldStatus": fridgeOpen,
-        "newStatus": nowOpen
+    let eventTimeStamp = moment();
+    restCallCounter = restCallCounter + 1;
+    let actionResponse = {
+        "wasOpen": fridgeOpen,
+        "isOpen": nowOpen,
+        "currentStatus": getOpenStatusString(),
+        "timestamp": formatMoment(eventTimeStamp)
     };
     fridgeOpen = nowOpen;
-    return response;
+
+    addToHistory(eventTimeStamp, fridgeOpen);
+
+    if(fridgeOpen) {
+        doorLastOpen = eventTimeStamp;
+    } else {
+        doorLastClosed = eventTimeStamp;
+    }
+    return actionResponse;
 }
 
 function log(message) {
@@ -50,8 +82,21 @@ function hasPendingAlarm() {
     return typeof alarmTimeout !== "undefined";
 }
 
+function getOpenStatusString() {
+    if(restCallCounter == 0) {
+        return "undefined";
+    } else if(fridgeOpen) {
+        return "open";
+    } else {
+        return "closed";
+    }
+}
+
 function getStatusObject() {
     let statusObj = {
+        "device": deviceName,
+        "currentStatus": getOpenStatusString(),
+        "version": packageJson.version,
         "updateCounter": restCallCounter,
         "doorOpenNow": fridgeOpen,
         "pendingAlarm": hasPendingAlarm(),
@@ -64,14 +109,48 @@ function getStatusObject() {
     if(typeof doorLastOpen !== "undefined") {
         statusObj.doorLastOpen = formatMoment(doorLastOpen);
     }
+    if(typeof doorLastClosed !== "undefined") {
+        statusObj.doorLastClosed = formatMoment(doorLastClosed);
+    }
     if(typeof lastAlarm !== "undefined") {
         statusObj.lastAlarm = formatMoment(lastAlarm);
     }
     return statusObj;
 }
 
+function validateSecretKey(req, res) {
+    const receivedKey = req.query.secretKey;
+    let errorObj;
+
+    console.log(req.params);
+
+    if(typeof receivedKey === "undefined") {
+        errorObj = errorObject("Secret key missing from REST call");
+    } else if(receivedKey !== secretKey) {
+        errorObj = errorObject("Invalid secret key received!");
+    } else {
+        return true;
+    }
+
+    log(errorObj.error);
+    res.status(403);
+    res.json(errorObj);
+    return false;
+}
+
+function errorObject(message) {
+    let errorObj = {
+        "error": message,
+        "time": formatCurrentMoment() 
+    }
+    return errorObj;
+}
+
 app.get('/', function (req, res) {
-    res.render('home', { 'status': getStatusObject() });
+    let renderDataObj = {};
+    renderDataObj.status = getStatusObject();
+    renderDataObj.history = eventHistory;
+    res.render('home', renderDataObj);
 });
 
 app.get("/status", (req, res, next) => {
@@ -81,8 +160,9 @@ app.get("/status", (req, res, next) => {
 
 app.get("/open", (req, res, next) => {
     log("Open call received");
-    restCallCounter = restCallCounter + 1;
-    doorLastOpen = moment();
+    if(!validateSecretKey(req, res)) {
+        return;
+    }
     let wasOpen = fridgeOpen;
     let resObj = setOpenStatus(true);
     res.json(resObj);
@@ -95,9 +175,11 @@ app.get("/open", (req, res, next) => {
     }
     });
 
-app.get("/close", (req, res, next) => {
-    log("Close call received");
-    restCallCounter = restCallCounter + 1;
+app.get("/closed", (req, res, next) => {
+    log("Closed call received");
+    if(!validateSecretKey(req, res)) {
+        return;
+    }
     if(hasPendingAlarm()) {
         clearTimeout(alarmTimeout);
         alarmTimeout = undefined;
@@ -105,6 +187,12 @@ app.get("/close", (req, res, next) => {
     let resObj = setOpenStatus(false);
     res.json(resObj);
    });
+
+app.get("/history", (req, res, next) => {
+    res.json(eventHistory);
+});
+
+app.use(express.static("public"));
 
 app.listen(3000, () => {
  log("Server running on port 3000");
